df4f5c36d4883bf7a4ca848933ca3de78abae6ad
GGolfz <wisarutgolf11@gmail.com>
2023-03-09 20:49:24 +0700

    refactor to move logic to applicaton layer

--- packages/parser/constant/password.ts
+++ packages/parser/constant/password.ts
@@ -1,6 +1,1 @@
-const MAX_NUMBER_OF_TRIES = 3;
-const TOO_MANY_PASSWORD_ATTEMPTS =
-  "Too many password attempts. Please try again later.";
-const ENTER_PASSWORD = "Please enter the password for the PDF file.";
-
-export { MAX_NUMBER_OF_TRIES, TOO_MANY_PASSWORD_ATTEMPTS, ENTER_PASSWORD };
+export const MAX_NUMBER_OF_TRIES = 3
