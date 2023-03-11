22920927f2ecf0df8b573461b63dde09cec105d3
jakapatb <max.jakkapat@gmail.com>
2023-03-11 20:32:38 +0700

    feat: ktc parser

--- packages/parser/index.ts
+++ packages/parser/index.ts
@@ -1,13 +1,14 @@
-import {DocumentSource} from './@types/DocumentSource'
-import {PasswordHandler} from './@types/PasswordHandler'
-import {Transaction} from './@types/Transaction'
-import {parseKasikornCreditStatement} from './parsers/kasikorn.credit'
-import {extractTextChunksFromPDF} from './utils/extractTextChunksFromPDF'
+import { DocumentSource } from "./@types/DocumentSource";
+import { PasswordHandler } from "./@types/PasswordHandler";
+import { Transaction } from "./@types/Transaction";
+import { parseKasikornCreditStatement } from "./parsers/kasikorn.credit";
+import { parseKTCCreditStatement } from "./parsers/ktc.credit";
+import { extractTextChunksFromPDF } from "./utils/extractTextChunksFromPDF";
 
-export type Bank = 'kasikorn'
-export type StatementType = 'credit' | 'account'
+export type Bank = "kasikorn" | "ktc";
+export type StatementType = "credit" | "account";
 
-export type {Transaction} from './@types/Transaction'
+export type { Transaction } from "./@types/Transaction";
 
 export async function parseStatement(
   bank: Bank,
@@ -15,11 +16,12 @@ export async function parseStatement(
   source: DocumentSource,
   passwordHandler: PasswordHandler
 ): Promise<Transaction[]> {
-  
-  const rawChunks = await extractTextChunksFromPDF(source, passwordHandler)
+  const rawChunks = await extractTextChunksFromPDF(source, passwordHandler);
 
-  if (bank === 'kasikorn' && type === 'credit')
-    return parseKasikornCreditStatement(rawChunks)
+  if (bank === "kasikorn" && type === "credit")
+    return parseKasikornCreditStatement(rawChunks);
+  if (bank === "ktc" && type === "credit")
+    return parseKTCCreditStatement(rawChunks);
 
-  throw new Error('statement type or bank is not supported')
+  throw new Error("statement type or bank is not supported");
 }
