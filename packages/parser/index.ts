4f89683804837380ef0c2a30299b791bc6175dd9
jakapatb <max.jakkapat@gmail.com>
2023-03-13 02:07:29 +0700

    feat: find bank & type from rawChunks

--- packages/parser/index.ts
+++ packages/parser/index.ts
@@ -4,33 +4,46 @@ import { Transaction } from "./@types/Transaction";
 
 import { parseKasikornCreditStatement } from "./parsers/kasikorn.credit";
 import { parseKTCCreditStatement } from "./parsers/ktc.credit";
-import { parseCitibankCreditStatement } from "./parsers/citibank.credit"
+import { parseCitibankCreditStatement } from "./parsers/citibank.credit";
 
 import { extractTextChunksFromPDF } from "./utils/extractTextChunksFromPDF";
 
-export type Bank = "kasikorn" | "ktc" | "citibank"
+const bankType = ["kasikorn", "ktc", "citibank"] as const;
+
+export type Bank = (typeof bankType)[number];
 export type StatementType = "credit" | "account";
 
 export type { Transaction } from "./@types/Transaction";
 
+const PATTERN_CREDIT = /(CREDIT CARD)/;
+const isCREDIT = (s: string) => PATTERN_CREDIT.test(s) && s.includes(".");
+
+const findStatementType = (s: string): StatementType => {
+  return isCREDIT(s) ? "credit" : "account";
+};
+
+const findBankType = (s: string): Bank => {
+  for (const bank of bankType) {
+    if (s.includes(bank)) return bank;
+  }
+};
+
 export async function parseStatement(
-  bank: Bank,
-  type: StatementType,
   source: DocumentSource,
   passwordHandler: PasswordHandler
 ): Promise<Transaction[]> {
   const rawChunks = await extractTextChunksFromPDF(source, passwordHandler);
+  const rawString = rawChunks.flat().join("");
+  const type = findStatementType(rawString);
+  const bank = findBankType(rawString);
+  console.log(bank, type);
+  if (type === "credit") {
+    if (bank === "kasikorn") return parseKasikornCreditStatement(rawChunks);
 
-	if (type === "credit") {
-		if (bank === "kasikorn")
-			return parseKasikornCreditStatement(rawChunks);
+    if (bank === "ktc") return parseKTCCreditStatement(rawChunks);
 
-		if (bank === "ktc")
-			return parseKTCCreditStatement(rawChunks);
-
-		if (bank === "citibank")
-			return parseCitibankCreditStatement(rawChunks)
-	}
+    if (bank === "citibank") return parseCitibankCreditStatement(rawChunks);
+  }
 
   throw new Error("statement type or bank is not supported");
 }
